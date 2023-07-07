7eb29c2383b87f0b1b352817d48633d15e635bd7
HectorM <hector_mndozer@hotmail.comm>
2023-07-06 20:12:18 -0600

    Commented text modified

--- Project 1/project1/libros/models.py
+++ Project 1/project1/libros/models.py
@@ -8,8 +8,8 @@ class Libros(models.Model):
         verbose_name_plural = "Libros"
 
     titulo = models.CharField("Título", max_length=300, default="Sin Nombrar")
-    edicion = models.CharField("Edición", max_length=300, default="Sin Específicar")
-    editorial = models.CharField("Editorial", max_length=300, default="Sin Específicar")
+    edicion = models.CharField("Edición", max_length=300, default="Sin Especificar")
+    editorial = models.CharField("Editorial", max_length=300, default="Sin Especificar")
     año_de_publicacion = models.IntegerField("Año de publicación", default=200)
     pagina = models.IntegerField("Número de páginas", blank=False)
 
