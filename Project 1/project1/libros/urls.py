4623e5ebd595a2f35f04a8d10e572cb2034d0cbd
HectorM <hector_mndozer@hotmail.comm>
2023-07-06 19:51:36 -0600

    Commented the path to insertar.libro in urls.py

--- Project 1/project1/libros/urls.py
+++ Project 1/project1/libros/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [ 
     path('', views.index, name='index'),
     path('inicio', views.Inicio.as_view(), name='inicio'),
-    path('insertar', views.insertar_libro, name='insertar'),
+    #path('insertar', views.insertar_libro, name='insertar'),
     path('formulario', views.Formulario.as_view(), name='formulario')
 ]
